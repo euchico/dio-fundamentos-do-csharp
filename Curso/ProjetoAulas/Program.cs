601142d1df175d913662504b3feb0214efaf8043
euchico <franciscodepaula.web@gmail.com>
2026-01-05 21:15:10 -0300

    docs: Atualização dos estudos.

---
+++ Curso/ProjetoAulas/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Primeiro Programa");
+
+int idade = 33;
+Console.WriteLine(idade);
+
+string nomePessoa = "Francisco de Paula";
+Console.WriteLine(nomePessoa);
+
+decimal valor = 200.99m;
+double valorDouble = 200.99;
+float valorFloat = 200.99f;
+Console.WriteLine(valor);
+Console.WriteLine(valorDouble);
+Console.WriteLine(valorFloat);
+
+var idadeNova = 34;
+
+char flag = 'Y';
+Console.WriteLine(flag);
+
+bool ativo = true;
+ativo = false;
+Console.WriteLine(ativo);
+
+const string descricao = "Curso de CSharp";
+Console.WriteLine(descricao);
